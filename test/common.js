2331dbd9c118308d94bdacdad6cd6ce318703b5a
Martin Krause <github@mkrause.info>
2017-10-06 22:53:49 +0200

    updates

--- test/common.js
+++ test/common.js
@@ -5,12 +5,5 @@ global.chai.should();
 
 global.expect = global.chai.expect;
 
-global.sinon = require("sinon");
-global.sinonChai = require("sinon-chai");
-global.chai.use(global.sinonChai);
-
-global.chaiAsPromised = require("chai-as-promised");
-global.chai.use(global.chaiAsPromised);
-
 global.ENV = "MOCHA";
 
